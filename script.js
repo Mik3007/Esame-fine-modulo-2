8ddb5296475ef041c51453689243008e38f9a2c7
Mik3007 <michelealtieri3007@gmail.com>
2024-03-04 17:40:28 +0100

    esame fine modulo 2

---
+++ script.js
@@ -0,0 +1,189 @@
+/*
+PARTE 1: 
+Oggi analizzeremo un problema molto comune: realizzare algoritmi di ricerca.
+Il tuo compito è creare una funzione che cercherà per posizione lavorativa E posizione geografica. Questi due valori verranno passati come parametri
+Ti abbiamo fornito un array chiamato "jobs" in fondo al file, NON modificarlo in alcun modo.
+L'algoritmo che devi realizzare cercherà SIA per posizione lavorativa che per posizione geografica.
+Prendi queste tre inserzioni ad esempio:
+
+      job1:  location: "NY, US",     title: "java dev"
+      job2:  location: "Genoa, IT"   title: "web dev"
+      job3:  location: "US"      title: "dev"
+
+Cercando contemporaneamente come posizione lavorativa "dev" e posizione geografica "US", dovresti ottenere come risultato solamente job1 e job3,
+in quanto job2 non soddisfa la condizione posta sulla posizione geografica.
+
+REQUISITI:
+- il tuo algoritmo deve tornare i risultati nella seguente forma:
+{
+  result: [], <-- inserisci qui le inserzioni che rispecchiano la posizione lavorativa e la posizione geografica richiesta
+  count: 0 <-- inserisci qui il numero totale delle inserzioni trovate
+}
+
+- la tua ricerca deve essere "case insensitive" (non deve essere influenzata da lettere maiuscole o minuscole nelle parole cercate). Questo e' possibile trasformando tutto in lettere minuscole con .toLowerCase()
+
+
+PARTE 2: 
+Nella pagina HTML, inserisci 2 input di tipo testo (uno per la location e uno per il titolo lavorativo, ricordati di diversificarli con un id) e un bottone con valore “cerca”
+
+Al click del bottone, il codice deve raccogliere i valori dei due input e darli in pasto alla funzione che hai creato nella parte 1. 
+
+Dopo aver raccolto ed elaborato i dati, e’ il momento di mostrare i risultati sulla pagina: 
+    Puoi scegliere tu se utilizzare un semplice ul / li oppure una tabella 
+    Vai passo per passo e usa molti console.log per capire eventualmente dove sbagli
+    SUGGERIMENTO: ti servira’ un ciclo for!
+
+*/
+
+// NON MODIFICARE QUESTO ARRAY!
+const jobs = [
+  { title: "Marketing Intern", location: "US, NY, New York" },
+  {
+    title: "Customer Service - Cloud Video Production",
+    location: "NZ, Auckland",
+  },
+  {
+    title: "Commissioning Machinery Assistant (CMA)",
+    location: "US, IA, Wever",
+  },
+  {
+    title: "Account Executive - Washington DC",
+    location: "US, DC, Washington",
+  },
+  { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
+  { title: "Accounting Clerk", location: "US, MD," },
+  { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
+  {
+    title: "Lead Guest Service Specialist",
+    location: "US, CA, San Francisco",
+  },
+  { title: "HP BSM SME", location: "US, FL, Pensacola" },
+  {
+    title: "Customer Service Associate - Part Time",
+    location: "US, AZ, Phoenix",
+  },
+  {
+    title: "ASP.net Developer Job opportunity at United States,New Jersey",
+    location: "US, NJ, Jersey City",
+  },
+  {
+    title: "Talent Sourcer (6 months fixed-term contract)",
+    location: "GB, LND, London",
+  },
+  {
+    title: "Applications Developer, Digital",
+    location: "US, CT, Stamford",
+  },
+  { title: "Installers", location: "US, FL, Orlando" },
+  { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
+  {
+    title: "VP of Sales - Vault Dragon",
+    location: "SG, 01, Singapore",
+  },
+  { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
+  {
+    title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
+    location: "GB, SOS, Southend-on-Sea",
+  },
+  { title: "Visual Designer", location: "US, NY, New York" },
+  {
+    title: "Process Controls Engineer - DCS PLC MS Office - PA",
+    location: "US, PA, USA Northeast",
+  },
+  { title: "Marketing Assistant", location: "US, TX, Austin" },
+  { title: "Front End Developer", location: "NZ, N, Auckland" },
+  { title: "Engagement Manager", location: "AE," },
+  {
+    title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
+    location: "US, CA, Carlsbad",
+  },
+  { title: "Customer Service", location: "GB, LND, London" },
+  { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
+  { title: "Marketing Exec", location: "SG," },
+  {
+    title: "HAAD/DHA Licensed Doctors Opening in UAE",
+    location: "AE, AZ, Abudhabi",
+  },
+  {
+    title: "Talent Management Process Manager",
+    location: "US, MO, St. Louis",
+  },
+  { title: "Customer Service Associate", location: "CA, ON, Toronto" },
+  {
+    title: "Customer Service Technical Specialist",
+    location: "US, MA, Waltham",
+  },
+  { title: "Software Applications Specialist", location: "US, KS," },
+  { title: "Craftsman Associate", location: "US, WA, Everett" },
+  { title: "Completion Engineer", location: "US, CA, San Ramon" },
+  { title: "I Want To Work At Karmarama", location: "GB, LND," },
+  {
+    title: "English Teacher Abroad",
+    location: "US, NY, Saint Bonaventure",
+  },
+];
+// creo una funzione per acquisire i campi inseriti dall'utente in base all'ID.
+
+function prendiElementoInput(elementId) {
+  return document.getElementById(elementId).value.toLowerCase().trim();
+}
+// creo una funzione con due varibili che utilizzino la funzione definita sopra per gli input dell'utente.
+
+function ricerca() {
+  const titolo = prendiElementoInput("title");
+  const localita = prendiElementoInput("location");
+
+  // effettuo una prima verifica, in modo che se l'utente non compila entrambi i dati, parta un alert!
+
+  if (titolo.length === 0 || localita.length === 0) {
+    alert("Compilare entrambi i campi");
+    return;
+  }
+
+  // creo una variabile e l'allego al div nell'htiml dove andrò alla fine ad inserire il risultato.
+
+  let risultato = document.getElementById("risultatoRicerca");
+
+  // creo due variabili, una dove andrà il risultato della ricerca, una per il contatore.
+  
+  console.log("Input titolo: ", titolo);
+  console.log("Input localita: ", localita);
+  let result = [];
+  let counter = 0;
+
+  // creo un forEach per ciclare tutti gli elementi dell'array rendendolo case insensitive e confrontando ogni elemento con l'input title e location inserito.
+
+  jobs.forEach(function (job) {
+    const jobTitle = job.title.toLowerCase();
+    const jobLocation = job.location.toLowerCase();
+    console.log(jobTitle);
+    console.log(jobLocation);
+
+    if (jobTitle.includes(titolo) && jobLocation.includes(localita)) {
+      result.push(job);
+    }
+  });
+  counter = result.length;
+
+  console.log(result);
+  console.log(counter);
+
+  risultato.innerHTML = "";
+
+  // creo un if per vedere se almeno un risultato è vero, se è c'è almeno un risultato parte la funzione.
+  // poi creo due variabili, una sarà la lista e una sarà gli elementi della list e alla fine con appenChild manderò tutto nel div risultatoRicerca.
+
+  if (result.length > 0) {
+    let lista = document.createElement("ol");
+
+    result.forEach(function (lavori) {
+      let elementiLista = document.createElement("li");
+      elementiLista.textContent = `${lavori.title} - ${lavori.location}`;
+      lista.appendChild(elementiLista);
+    });
+    risultato.appendChild(lista);
+  } else {
+    risultato.textContent =
+      "nessuna posiziona lavorativa trovata, DISOCCUPATO PER SEMPRE!";
+  }
+}
